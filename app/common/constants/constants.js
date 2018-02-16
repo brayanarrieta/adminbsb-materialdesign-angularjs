61e4eef9b1eca3f4e8a1a3fa34a6e8aea0dd9893
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-02-16 13:46:40 -0600

    Changes in scaffolding

---
+++ app/common/constants/constants.js
@@ -0,0 +1,7 @@
+'use strict';
+(function () {
+    angular.module('app')
+        .constant('APP_AUTHOR', 'Brayan Arrieta')
+        .constant('APP_NAME', 'adminbsb-materialdesign-angularjs')
+        .constant('APP_VERSION', '1.0.0');
+})();
